d77262f3f6c88a93072e819231dcd1c0d4dde6eb
Nat89 <grupaa.nowak@gmail.com>
2019-02-20 11:54:16 +0100

    11:54

---
+++ src/Algorytmy.java
@@ -0,0 +1,13 @@
+public class Algorytmy {
+    public static void main(String[] args) {
+        Main a = new Main();
+        System.out.println("Wynik potęgowania: " + a.power(3, 4));
+        System.out.println("Wynik silni: " + a.factorial (4));
+        System.out.println("Suma cg: " + a.geoSequence (2,2,3));
+        System.out.println("Suma cg: " + a.algSequence (2,2,4));
+        System.out.println("Czy doskonała: " + a.perfectNum (6));
+        System.out.println("Czy doskonała: " + a.perfectNum (11));
+        System.out.println("Czy doskonała: " + a.perfectNum (28));
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Algorytmy");
+    // algorytm potęgowania
+    public int power (int x, int y) {
+
+        int result = 1;
+        // 3
+        for (int i = 1; i <= y; i++) {
+            result = result * x;
+        }
+        return result;
+    }
+        // algorytm silnia z n
+        public int factorial (int n) {
+            // 2
+            int result = 1;
+            // 3 }
+            for (int i = n; i > 1; i--) {
+                result = result * i;
+
+            }
+
+        return result;
+
+    }
+    // algorytm ciągu geometrycznego
+    public int geoSequence(int a0, int q, int n) {
+        int sum = 0;
+        for (int i = 0; i <= n-1; i++) {
+            sum = sum + (a0 * power(q, i));
+
+        }
+        return sum;
+
+    }
+    public int algSequence (int a0, int r, int n) {
+        int sum = 0;
+        for (int i = 0; i <= n-1; i++) {
+            sum = sum + (a0 + i*r);
+
+        }
+        return  sum;
+
+    }
+    public boolean perfectNum(int num) {
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum = sum + i;
+            }
+        }
+        return num == sum ? true : false;
     }
 }
